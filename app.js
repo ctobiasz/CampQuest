ec46a9ce7e0ffd51d4606cbcc8a3af80632b0307
Christopher Tobiasz <tobiasz.christopher@gmail.com>
2019-05-09 10:46:04 -0400

    linked stylesheet and added css to the comments

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var seedDB     = require("./seeds");
 mongoose.connect("mongodb://localhost:27017/camp_fire", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 app.get("/", (req, res) => {
