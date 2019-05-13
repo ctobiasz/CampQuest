e567cd9ba9531875fb206779bebb8f919ee61fdf
Christopher Tobiasz <tobiasz.christopher@gmail.com>
2019-05-13 18:36:24 -0400

    assossicatedcampgrounds with user creator

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,23 +14,29 @@ router.get("/", (req,res ) => {
    });
 });
 //CREATE - add new campground to DB
-router.post("/", (req,res) => {
+router.post("/", isLoggedIn, (req,res) => {
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
-  var newCampground = {name: name, image: image, description: desc}
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  }
+  var newCampground = {name: name, image: image, description: desc, author: author}
+
   // Create a new campground and sae to DB
    Campground.create(newCampground, function(err, newlyCreated){
      if(err){
        console.log(err);
      } else {
-       //redurect back to campgrounds page
+       //redirect back to campgrounds page
+       console.log(newlyCreated);
        res.redirect("/campgrounds");
      }
    });
 });
 //NEW  - show form to create new campground
-router.get("/new", (req,res) => {
+router.get("/new", isLoggedIn, (req,res) => {
   res.render("campgrounds/new")
 });
 // SHOW - shows more info about one campground
@@ -45,5 +51,13 @@ router.get("/:id", (req,res) => {
     }
   });
 });
+
+//middleware
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+};
 
 module.exports = router;
