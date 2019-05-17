8fb508533de7a1baa2a7d8321357161268dd296f
Christopher Tobiasz <tobiasz.christopher@gmail.com>
2019-05-16 21:23:50 -0400

    users can now delete/edit their posts

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -53,18 +53,14 @@ router.get("/:id", (req,res) => {
 });
 
 //EDIT CAMPGROUNDS ROUTE
-router.get("/:id/edit", (req,res) => {
-   Campground.findById(req.params.id, function(err, foundCampground){
-     if(err){
-       res.redirect("/campgrounds");
-     } else {
-       res.render("campgrounds/edit", {campground: foundCampground});
-     }
-   });
+router.get("/:id/edit", checkCampgroundOwnership,(req,res) => {
+    Campground.findById(req.params.id, function(err, foundCampground){
+      res.render("campgrounds/edit", {campground: foundCampground});
+    });
 });
 
 //UPDATE CAMPGROUND ROUTE
-router.put("/:id", (req,res)=>{
+router.put("/:id", checkCampgroundOwnership,(req,res)=>{
   //find and update the correct campground
   Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
     if(err){
@@ -76,12 +72,43 @@ router.put("/:id", (req,res)=>{
   //redirect somewhere
 });
 
+//DESTROY CAMPGROUND ROUTE
+router.delete("/:id", checkCampgroundOwnership,(req,res) => {
+  Campground.findByIdAndRemove(req.params.id, function(err){
+   if(err){
+     res.redirect("/campgrounds");
+   } else {
+     res.redirect("/campgrounds");
+   }
+ });
+});
+
+
 //middleware
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
     return next();
   }
   res.redirect("/login");
+};
+
+function checkCampgroundOwnership(req, res, next){
+  if(req.isAuthenticated()) {
+    Campground.findById(req.params.id, function(err, foundCampground){
+      if(err){
+        res.redirect("back");
+      } else {
+        //does user own the campground?
+        if(foundCampground.author.id.equals(req.user._id)){
+          next();
+        } else {
+           res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
 };
 
 module.exports = router;
