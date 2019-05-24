04431402524e63dcfcd095cd564243f3f92785f3
Christopher Tobiasz <tobiasz.christopher@gmail.com>
2019-05-23 23:23:52 -0400

    ....

--- models/campground.js
+++ models/campground.js
@@ -6,7 +6,7 @@ var campgroundSchema = new mongoose.Schema({
   name: String,
   image: String,
   description: String,
-  price: Number,
+  cost: Number,
   location: String,
   lat: Number,
   lng: Number,
